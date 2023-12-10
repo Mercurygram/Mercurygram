d307607be47aa99db83b7fdb1b05745fe22e3f4a
Timothy Redaelli <timothy@fsfe.org>
2023-12-10 15:46:55 +0100

    [MG] update to 10.3.2 (4145)
    Squashed commit of the following:
    
    commit 33a48d8945654afdb99df778647251cc2fca4bcc
    Author: dkaraush <dkaraush@gmail.com>
    Date:   Mon Dec 4 22:17:50 2023 +0400
    
        update to 10.3.2 (4145)
    
    commit 220f6b4d73793c57ba6f9023b8d38d171f342a7e
    Author: dkaraush <dkaraush@gmail.com>
    Date:   Wed Nov 15 16:23:35 2023 +0400
    
        update to 10.2.9 (4087)

--- TMessagesProj/src/main/java/org/telegram/ui/PaymentFormActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PaymentFormActivity.java
@@ -3372,6 +3372,8 @@ public class PaymentFormActivity extends BaseFragment implements NotificationCen
                                 paymentFormCallback.onInvoiceStatusChanged(invoiceStatus);
                             }
                             finishFragment();
+                        } else if (invoiceStatus == InvoiceStatus.PAID && !isFinishing()) {
+                            finishFragment();
                         }
                     }, 500);
                 }
