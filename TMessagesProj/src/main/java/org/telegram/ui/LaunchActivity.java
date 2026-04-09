e6dd401a5ceca9209e82f1adf6e626acc35e9c6f
Timothy Redaelli <timothy@fsfe.org>
2026-04-09 20:49:07 +0200

    [MG] re-add Monet themes (Material You dynamic colors)

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -253,6 +253,8 @@ import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import tw.nekomimi.nekogram.helpers.MonetHelper;
+
 public class LaunchActivity extends BasePermissionsActivity implements INavigationLayout.INavigationLayoutDelegate, NotificationCenter.NotificationCenterDelegate, DialogsActivity.DialogsActivityDelegate, IPipActivity {
     public final static String EXTRA_FORCE_NOT_INTERNAL_APPS = "force_not_internal_apps";
     public final static String EXTRA_FORCE_REQUEST = "force_request";
@@ -703,6 +705,10 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         // Warn existing ntfy.sh users on first launch after this feature was added
         if (SharedConfig.isNtfyDefaultServer()) {
             NotificationsSettingsActivity.showNtfyDefaultServerDialog(this);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MonetHelper.registerReceiver(this);
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -6866,6 +6872,9 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
     protected void onDestroy() {
         isActive = false;
         activeInstanceCount--;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MonetHelper.unregisterReceiver(this);
+        }
         unregisterReceiver(batteryReceiver);
 
         if (activeInstanceCount == 0) {
