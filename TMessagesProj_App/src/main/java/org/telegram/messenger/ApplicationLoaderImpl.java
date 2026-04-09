dc55368e364db2df435743df571a7d6df165f249
Timothy Redaelli <timothy@fsfe.org>
2026-04-09 20:49:07 +0200

    [MG] add release workflow and script

--- TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
+++ TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
@@ -1,6 +1,23 @@
 package org.telegram.messenger;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.view.ViewGroup;
+
 import org.telegram.messenger.regular.BuildConfig;
+import org.telegram.tgnet.TLRPC;
+import org.telegram.ui.Components.AlertsCreator;
+import org.telegram.ui.Components.MgUpdateLayout;
+import org.telegram.ui.IUpdateLayout;
+
+import java.io.File;
+
+import it.belloworld.mercurygram.MgUpdateChecker;
+import it.belloworld.mercurygram.MgUpdateInfo;
+import it.belloworld.mercurygram.ui.MgUpdateAlertDialog;
 
 public class ApplicationLoaderImpl extends ApplicationLoader {
     @Override
@@ -26,5 +43,50 @@ public class ApplicationLoaderImpl extends ApplicationLoader {
     @Override
     protected String onGetApplicationId() {
         return BuildConfig.APPLICATION_ID;
+    }
+
+    @Override
+    protected void checkForUpdatesInternal() {
+        MgUpdateChecker.checkForUpdates(false);
+    }
+
+    @Override
+    public boolean checkApkInstallPermissions(final Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !ApplicationLoader.applicationContext.getPackageManager().canRequestPackageInstalls()) {
+            AlertsCreator.createApkRestrictedDialog(context, null).show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean openApkInstall(Activity activity, TLRPC.Document document) {
+        File apk = MgUpdateChecker.getUpdateApkFile();
+        if (apk != null) {
+            MgUpdateChecker.installUpdate(activity, apk);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean showUpdateAppPopup(Context context, TLRPC.TL_help_appUpdate update, int account) {
+        if (SharedConfig.isMgUpdateAvailable()) {
+            try {
+                MgUpdateInfo info = SharedConfig.getMgPendingUpdate();
+                if (info != null) {
+                    new MgUpdateAlertDialog(context, info).show();
+                    return true;
+                }
+            } catch (Exception e) {
+                FileLog.e(e);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public IUpdateLayout takeUpdateLayout(Activity activity, ViewGroup sideMenuContainer) {
+        return new MgUpdateLayout(activity, sideMenuContainer);
     }
 }
