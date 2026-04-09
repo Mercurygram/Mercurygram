6a496e6165edafa005a3b821d412472fdf6b2c74
Timothy Redaelli <timothy@fsfe.org>
2026-04-09 20:49:04 +0200

    [TF] remove GMS/Firebase/MLKit/Cast/Passkeys/libgsaverification from sources, gradle and manifests

--- TMessagesProj/src/main/java/org/telegram/ui/Components/AudioPlayerAlert.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/AudioPlayerAlert.java
@@ -68,7 +68,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import androidx.media3.common.C;
-import com.google.android.gms.cast.framework.CastContext;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.ApplicationLoader;
@@ -1097,13 +1096,7 @@ public class AudioPlayerAlert extends BottomSheet implements NotificationCenter.
                 }
             }
         };
-        castAvailable = true;
-        try {
-            castItemButton.setRouteSelector(CastContext.getSharedInstance(context).getMergedSelector());
-        } catch (Exception e) {
-            FileLog.e(e);
-            castAvailable = false;
-        }
+        castAvailable = false;
         castItemButton.setVisibility(View.INVISIBLE);
         if (optionsIcon != null) {
             optionsIcon.setCasting(CastSync.isActive(), true);
