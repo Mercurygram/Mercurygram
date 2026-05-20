72c51571101b32b7826e4832020a0219bea8e137
Timothy Redaelli <timothy@fsfe.org>
2026-05-20 21:03:25 +0200

    [MG] add option to hide All Chats tab

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -65,6 +65,7 @@ public class UserConfig extends BaseController {
     public boolean sendLargePhotos = false;
     public boolean rearRoundCamera = false;
     public boolean hideChatKeyboard = false;
+    public boolean hideAllTab = false;
     public boolean hasSecureData;
     public int loginTime;
     public TLRPC.TL_help_termsOfService unacceptedTermsOfService;
@@ -168,6 +169,7 @@ public class UserConfig extends BaseController {
                     editor.putBoolean("sendLargePhotos", sendLargePhotos);
                     editor.putBoolean("rearRoundCamera", rearRoundCamera);
                     editor.putBoolean("hideChatKeyboard", hideChatKeyboard);
+                    editor.putBoolean("hideAllTab", hideAllTab);
                     editor.putBoolean("hasSecureData", hasSecureData);
                     editor.putBoolean("notificationsSettingsLoaded4", notificationsSettingsLoaded);
                     editor.putBoolean("notificationsSignUpSettingsLoaded", notificationsSignUpSettingsLoaded);
@@ -323,6 +325,7 @@ public class UserConfig extends BaseController {
             sendLargePhotos = preferences.getBoolean("sendLargePhotos", false);
             rearRoundCamera = preferences.getBoolean("rearRoundCamera", false);
             hideChatKeyboard = preferences.getBoolean("hideChatKeyboard", false);
+            hideAllTab = preferences.getBoolean("hideAllTab", false);
             hasSecureData = preferences.getBoolean("hasSecureData", false);
             notificationsSettingsLoaded = preferences.getBoolean("notificationsSettingsLoaded4", false);
             notificationsSignUpSettingsLoaded = preferences.getBoolean("notificationsSignUpSettingsLoaded", false);
@@ -498,6 +501,7 @@ public class UserConfig extends BaseController {
         sendLargePhotos = false;
         rearRoundCamera = false;
         hideChatKeyboard = false;
+        hideAllTab = false;
         unreadDialogsLoaded = true;
         hasValidDialogLoadIds = true;
         unacceptedTermsOfService = null;
--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -3452,6 +3452,13 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                 updateStatus(UserConfig.getInstance(currentAccount).getCurrentUser(), false);
             }
             if (folderId == 0) {
+                actionBar.setOnLongClickListener(v -> {
+                    if (getUserConfig().hideAllTab && filterTabsView != null && filterTabsView.getDefaultTabId() != filterTabsView.getCurrentTabId()) {
+                        filterTabsView.toggleAllTabs(true);
+                        filterTabsView.selectDefaultTab();
+                    }
+                    return false;
+                });
                 actionBar.setSupportsHolidayImage(true);
             }
         }
@@ -6744,7 +6751,9 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                 filterTabsView.removeTabs();
                 for (int a = 0, N = filters.size(); a < N; a++) {
                     if (filters.get(a).isDefault()) {
-                        filterTabsView.addTab(a, 0, LocaleController.getString(R.string.FilterAllChats), null, false, true, filters.get(a).locked);
+                        if (filterTabsView.showAllChatsTab) {
+                            filterTabsView.addTab(a, 0, LocaleController.getString(R.string.FilterAllChats), null, false, true, filters.get(a).locked);
+                        }
                     } else {
                         final MessagesController.DialogFilter filter = filters.get(a);
                         filterTabsView.addTab(a, filter.localId, filter.name, filter.entities, filter.title_noanimate, false, filters.get(a).locked);
@@ -6773,6 +6782,17 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                     viewPages[a].listView.setScrollingTouchSlop(RecyclerView.TOUCH_SLOP_PAGING);
                 }
                 filterTabsView.finishAddingTabs(animatedUpdateItems);
+                // [MG] When "All Chats" tab is hidden, sync viewPages[0] with the
+                // actually-selected tab id so switchToCurrentSelectedMode() resolves
+                // the correct (non-default) filter on initial load.
+                if (!filterTabsView.showAllChatsTab) {
+                    int currentTabId = filterTabsView.getCurrentTabId();
+                    if (currentTabId >= 0 && currentTabId < filters.size()
+                            && viewPages[0].selectedType != currentTabId) {
+                        viewPages[0].selectedType = currentTabId;
+                        updateCurrentTab = true;
+                    }
+                }
                 if (updateCurrentTab) {
                     switchToCurrentSelectedMode(false);
                 }
@@ -7168,7 +7188,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             }
             return false;
         } else if (filterTabsView != null && filterTabsView.getVisibility() == View.VISIBLE && !tabsAnimationInProgress && !filterTabsView.isAnimatingIndicator() && !startedTracking && !filterTabsView.isFirstTabSelected()) {
-            if (invoked) filterTabsView.selectFirstTab();
+            if (invoked && !getUserConfig().hideAllTab) filterTabsView.selectFirstTab();
             return false;
         } else if (commentView != null && commentView.isPopupShowing()) {
             if (invoked) commentView.hidePopup(true);
