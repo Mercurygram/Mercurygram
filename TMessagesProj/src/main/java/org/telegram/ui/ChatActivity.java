2af4dab46549475d08d856ae86f2eb70f4b22384
Timothy Redaelli <timothy@fsfe.org>
2026-05-20 21:03:10 +0200

    [MG] hide keyboard on chat scroll

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -6802,6 +6802,9 @@ public class ChatActivity extends BaseFragment implements
                         wasManualScroll = true;
                         scrollingChatListView = true;
                     } else if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                        if (getUserConfig().mg.hideChatKeyboard) {
+                            AndroidUtilities.hideKeyboard(getParentActivity().getCurrentFocus());
+                        }
                         pollHintCell = null;
                         wasManualScroll = true;
                         scrollingFloatingDate = true;
