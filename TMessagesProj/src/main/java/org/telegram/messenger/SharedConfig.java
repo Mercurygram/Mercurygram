Foam0/telegram-battery-client

[MG] add custom emoji pack importer
Side-load a user-supplied emoji pack at runtime instead of the bundled
Noto set. Accepts a .zip of per-glyph PNGs (<page>_<index>.png) or a raw
Telegram .apk (emoji at assets/emoji/), matched on basename so any layout
works. Nothing proprietary enters the repo/APK and the build processes no
new assets — F-Droid reproducibility untouched (same "user vouches"
posture as the whisper-model manual import).

Lookup is lenient + per-glyph: a missing glyph falls back to the bundled
asset, so a partial pack — or an EmojiData index shift after an upstream
rebase — renders a few built-in glyphs rather than blanking.

- MgEmojiPack: import/storage (filesDir/mg_emoji), per-glyph load routed
  from Emoji.loadEmoji, memoized installed-count.
- Emoji.clearEmojiCache + cacheEpoch guard so an in-flight load decoded
  from the previous source can't cache a stale glyph after the toggle
  flips / a pack is imported or removed.
- mg_useCustomEmojiPack global flag (process-wide bitmap cache).
- Settings: MercurygramEmojiSettingsActivity sub-screen + a row in
  MercurygramSettingsActivity; row subtitle reports "No pack installed"
  when enabled-but-empty so it never implies custom emoji are active.

TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
@@ -277,6 +277,17 @@ public class SharedConfig {
                 .apply();
     }
 
+    public static void toggleMgUseCustomEmojiPack() {
+        mg_useCustomEmojiPack = !mg_useCustomEmojiPack;
+        ApplicationLoader.applicationContext.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE)
+                .edit()
+                .putBoolean("mg_useCustomEmojiPack", mg_useCustomEmojiPack)
+                .apply();
+        // Drop cached glyph bitmaps so already-rendered spans repaint from the
+        // newly-selected source on the next frame.
+        Emoji.clearEmojiCache();
+    }
+
     public static void setMgTranslateAltEngine(String engine) {
         engine = sanitizeMgTranslateAltEngine(engine);
         mg_translateAltEngine = engine;
@@ -589,6 +600,12 @@ public class SharedConfig {
     public static boolean disableAutoUpdate = false;
     public static boolean acceptPreReleaseUpdates = false;
     public static boolean useSystemFont = false;
+    // Mercurygram: when on, Emoji rendering loads user-supplied glyphs from a
+    // side-loaded pack (it.belloworld.mercurygram.emoji.MgEmojiPack) instead of
+    // the bundled Noto set, falling back per-glyph to the bundle for any glyph
+    // the pack is missing. Global because the emoji bitmap cache (Emoji.emojiBmp)
+    // is a process-wide static, same as useSystemFont/useSystemEmoji.
+    public static boolean mg_useCustomEmojiPack = false;
 
     // Mercurygram: Privacy
     public static boolean reduceTrackingFingerprint = false;
@@ -1223,6 +1240,7 @@ public class SharedConfig {
             mg_transcribeOffline = preferences.getBoolean("mg_transcribeOffline", false);
             mg_transcribeModel = preferences.getString("mg_transcribeModel", "tiny-q8_0");
             mg_transcribeVad = preferences.getBoolean("mg_transcribeVad", true);
+            mg_useCustomEmojiPack = preferences.getBoolean("mg_useCustomEmojiPack", false);
             migratePerAccountSettingsV1(preferences);
             migrateTranscribeLangToPerAccount(preferences);
             migrateHideStoriesToPerAccount(preferences);

Mercurygram/Mercurygram

TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
@@ -289,6 +289,17 @@ public class SharedConfig {
                 .apply();
     }
 
+    public static void toggleMgUseCustomEmojiPack() {
+        mg_useCustomEmojiPack = !mg_useCustomEmojiPack;
+        ApplicationLoader.applicationContext.getSharedPreferences("userconfing", Context.MODE_PRIVATE)
+                .edit()
+                .putBoolean("mg_useCustomEmojiPack", mg_useCustomEmojiPack)
+                .apply();
+        // Drop cached glyph bitmaps so already-rendered spans repaint from the
+        // newly-selected source on the next frame.
+        Emoji.clearEmojiCache();
+    }
+
     public static void setMgTranslateAltEngine(String engine) {
         engine = sanitizeMgTranslateAltEngine(engine);
         mg_translateAltEngine = engine;
@@ -655,6 +666,12 @@ public class SharedConfig {
     // above) apart from a fresh opt-in that has never been on a prerelease.
     public static String mgLastPreReleaseTag = "";
     public static boolean useSystemFont = false;
+    // Mercurygram: when on, Emoji rendering loads user-supplied glyphs from a
+    // side-loaded pack (it.belloworld.mercurygram.emoji.MgEmojiPack) instead of
+    // the bundled Noto set, falling back per-glyph to the bundle for any glyph
+    // the pack is missing. Global because the emoji bitmap cache (Emoji.emojiBmp)
+    // is a process-wide static, same as useSystemFont/useSystemEmoji.
+    public static boolean mg_useCustomEmojiPack = false;
 
     // Mercurygram: Privacy
     public static boolean reduceTrackingFingerprint = false;
@@ -1007,6 +1024,7 @@ public class SharedConfig {
         editor.putBoolean("mg_transcribeOffline", mg_transcribeOffline);
         editor.putString("mg_transcribeModel", mg_transcribeModel);
         editor.putBoolean("mg_transcribeVad", mg_transcribeVad);
+        editor.putBoolean("mg_useCustomEmojiPack", mg_useCustomEmojiPack);
         editor.putString("mg_webPushPrivateKey", webPushPrivateKey != null ? Base64.encodeToString(webPushPrivateKey, Base64.DEFAULT) : "");
         editor.putString("mg_webPushPublicKey", webPushPublicKey != null ? Base64.encodeToString(webPushPublicKey, Base64.DEFAULT) : "");
         editor.putString("mg_webPushAuthSecret", webPushAuthSecret != null ? Base64.encodeToString(webPushAuthSecret, Base64.DEFAULT) : "");
@@ -1061,6 +1079,7 @@ public class SharedConfig {
         mg_transcribeOffline = preferences.getBoolean("mg_transcribeOffline", false);
         mg_transcribeModel = preferences.getString("mg_transcribeModel", "tiny-q8_0");
         mg_transcribeVad = preferences.getBoolean("mg_transcribeVad", true);
+        mg_useCustomEmojiPack = preferences.getBoolean("mg_useCustomEmojiPack", false);
         migratePerAccountSettingsV1(preferences);
         migrateTranscribeLangToPerAccount(preferences);
         migrateHideStoriesToPerAccount(preferences);