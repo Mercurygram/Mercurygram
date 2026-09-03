Foam0/telegram-battery-client

[MG] add Tor plugin (companion APK) + main integration
OCCRP / Symbolic Software (May 2026) showed MTProto's outer obfuscation2
stream cipher is recoverable from on-wire bytes — a passive observer can
read the 8-byte auth_key_id at the front of every frame. The previous
commit added handshake-side mitigations (per-network rotation, CDN refusal,
1h temp-key TTL). This commit adds the last mitigation in the OCCRP set:
routing MTProto through an embedded Tor daemon.

Tor lives in a companion APK (it.belloworld.mercurygram.plugin.tor,
Gradle module :TMessagesProj_PluginTor) released from the same repo, same
tag, and same signing key as main. The split keeps libmgtor + the geoip
blobs (~25–27 MB per ABI) out of every install — only users who actually
turn the toggle on download the plugin.

* Plugin (:TMessagesProj_PluginTor)

  Wraps the existing MgTorController + MgTorBootstrap + MgTorNative tor
  runtime in an AIDL Service (IMgTorService / IMgTorCallback) running in
  its own :tor process inside a foreground Service. OOM/lifecycle is
  decoupled from the messenger UI. Idle-stop / VoIP-keepalive /
  UnifiedPush fallback semantics unchanged — only the IPC boundary is
  new.

  Native build (jni/build_{openssl,libevent,tor}.sh + CMakeLists.txt)
  statically links tor + libevent + an in-tree OpenSSL fork (tor needs
  APIs BoringSSL removed) into libmgtor.so. Kept separate from
  libtmessages.so so the two C++ crypto stacks cannot collide via
  dynamic-symbol interposition (both export EVP_*, SSL_*, BIO_* under
  the same C ABI). Submodules pinned by SHA in .gitmodules at the
  TMessagesProj_PluginTor/jni/ paths.

  Control port is gated by --CookieAuthentication 1 + --CookieAuthFile
  so a co-resident app with INTERNET permission cannot loopback-AUTH
  and issue SIGNAL SHUTDOWN / NEWNYM / hijack SOCKS / GETINFO
  fingerprint our circuit state. MgTorBootstrap reads the 32-byte
  cookie (poll loop, 15 s budget) and sends AUTHENTICATE <hex>; the
  stale cookie is deleted on 

TMessagesProj_AppTests/src/androidTest/kotlin/it/belloworld/mercurygram/tor/MgUseTorToggleTest.kt
@@ -1,0 +1,89 @@
+package it.belloworld.mercurygram.tor
+
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.telegram.messenger.ApplicationLoader
+import org.telegram.messenger.SharedConfig
+
+class MgUseTorToggleTest {
+
+    @Before
+    fun setUp() {
+        ensureAppContext()
+        if (SharedConfig.mg_useTor) SharedConfig.toggleMgUseTor()
+    }
+
+    @After
+    fun tearDown() {
+        if (SharedConfig.mg_useTor) SharedConfig.toggleMgUseTor()
+    }
+
+    @Test
+    fun togglePersistsBoolean() {
+        assertFalse(SharedConfig.mg_useTor)
+        SharedConfig.toggleMgUseTor()
+        assertTrue(SharedConfig.mg_useTor)
+
+        val prefs = InstrumentationRegistry.getInstrumentation().targetContext
+            .getSharedPreferences("mainconfig", Context.MODE_PRIVATE)
+        assertTrue(prefs.getBoolean("mg_useTor", false))
+
+        SharedConfig.toggleMgUseTor()
+        assertFalse(SharedConfig.mg_useTor)
+        assertFalse(prefs.getBoolean("mg_useTor", true))
+    }
+
+    @Test
+    fun idleTimeoutRearmTransitionDoesNotThrow() {
+        // 0 -> N>0 transition reaches into MgTorClient.resumeIdleTickerIfNeeded
+        // (post-plugin-extraction this is a stub that forwards to the bound
+        // service when one exists). With no plugin installed in the
+        // instrumentation environment the call must return without scheduling
+        // — the setter must not throw or leak a ticker into the test process.
+        val original = SharedConfig.mg_torIdleStopMinutes
+        try {
+            SharedConfig.setMgTorIdleStopMinutes(0)
+            assertEquals(0, SharedConfig.mg_torIdleStopMinutes)
+            SharedConfig.setMgTorIdleStopMinutes(15)
+            assertEquals(15, SharedConfig.mg_torIdleStopMinutes)
+            // No assertion on the ticker itself — the public surface is the
+            // pref. Smoke test: the call chain must complete without errors.
+            // (State is not asserted: the MgTorClient singleton survives
+            // across tests, and a preceding preInit call from another test
+            // may have moved it out of UNKNOWN.)
+        } finally {
+            SharedConfig.setMgTorIdleStopMinutes(original)
+        }
+    }
+
+    @Test
+    fun idleTimeoutMinutesPersists() {
+        val original = SharedConfig.mg_torIdleStopMinutes
+        try {
+            SharedConfig.setMgTorIdleStopMinutes(15)
+            assertEquals(15, SharedConfig.mg_torIdleStopMinutes)
+            val prefs = InstrumentationRegistry.getInstrumentation().targetContext
+                .getSharedPreferences("mainconfig", Context.MODE_PRIVATE)
+            assertEquals(15, prefs.getInt("mg_torIdleStopMinutes", -1))
+
+            // Negative input clamps to 0 (matches "always on" semantics in UI).
+            SharedConfig.setMgTorIdleStopMinutes(-3)
+            assertEquals(0, SharedConfig.mg_torIdleStopMinutes)
+        } finally {
+            SharedConfig.setMgTorIdleStopMinutes(original)
+        }
+    }
+
+    private fun ensureAppContext() {
+        if (ApplicationLoader.applicationContext == null) {
+            ApplicationLoader.applicationContext =
+                InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+        }
+    }
+}

Mercurygram/Mercurygram

TMessagesProj_AppTests/src/androidTest/kotlin/it/belloworld/mercurygram/tor/MgUseTorToggleTest.kt
@@ -1,0 +1,89 @@
+package it.belloworld.mercurygram.tor
+
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.telegram.messenger.ApplicationLoader
+import org.telegram.messenger.SharedConfig
+
+class MgUseTorToggleTest {
+
+    @Before
+    fun setUp() {
+        ensureAppContext()
+        if (SharedConfig.mg_useTor) SharedConfig.toggleMgUseTor()
+    }
+
+    @After
+    fun tearDown() {
+        if (SharedConfig.mg_useTor) SharedConfig.toggleMgUseTor()
+    }
+
+    @Test
+    fun togglePersistsBoolean() {
+        assertFalse(SharedConfig.mg_useTor)
+        SharedConfig.toggleMgUseTor()
+        assertTrue(SharedConfig.mg_useTor)
+
+        val prefs = InstrumentationRegistry.getInstrumentation().targetContext
+            .getSharedPreferences("userconfing", Context.MODE_PRIVATE)
+        assertTrue(prefs.getBoolean("mg_useTor", false))
+
+        SharedConfig.toggleMgUseTor()
+        assertFalse(SharedConfig.mg_useTor)
+        assertFalse(prefs.getBoolean("mg_useTor", true))
+    }
+
+    @Test
+    fun idleTimeoutRearmTransitionDoesNotThrow() {
+        // 0 -> N>0 transition reaches into MgTorClient.resumeIdleTickerIfNeeded
+        // (post-plugin-extraction this is a stub that forwards to the bound
+        // service when one exists). With no plugin installed in the
+        // instrumentation environment the call must return without scheduling
+        // — the setter must not throw or leak a ticker into the test process.
+        val original = SharedConfig.mg_torIdleStopMinutes
+        try {
+            SharedConfig.setMgTorIdleStopMinutes(0)
+            assertEquals(0, SharedConfig.mg_torIdleStopMinutes)
+            SharedConfig.setMgTorIdleStopMinutes(15)
+            assertEquals(15, SharedConfig.mg_torIdleStopMinutes)
+            // No assertion on the ticker itself — the public surface is the
+            // pref. Smoke test: the call chain must complete without errors.
+            // (State is not asserted: the MgTorClient singleton survives
+            // across tests, and a preceding preInit call from another test
+            // may have moved it out of UNKNOWN.)
+        } finally {
+            SharedConfig.setMgTorIdleStopMinutes(original)
+        }
+    }
+
+    @Test
+    fun idleTimeoutMinutesPersists() {
+        val original = SharedConfig.mg_torIdleStopMinutes
+        try {
+            SharedConfig.setMgTorIdleStopMinutes(15)
+            assertEquals(15, SharedConfig.mg_torIdleStopMinutes)
+            val prefs = InstrumentationRegistry.getInstrumentation().targetContext
+                .getSharedPreferences("userconfing", Context.MODE_PRIVATE)
+            assertEquals(15, prefs.getInt("mg_torIdleStopMinutes", -1))
+
+            // Negative input clamps to 0 (matches "always on" semantics in UI).
+            SharedConfig.setMgTorIdleStopMinutes(-3)
+            assertEquals(0, SharedConfig.mg_torIdleStopMinutes)
+        } finally {
+            SharedConfig.setMgTorIdleStopMinutes(original)
+        }
+    }
+
+    private fun ensureAppContext() {
+        if (ApplicationLoader.applicationContext == null) {
+            ApplicationLoader.applicationContext =
+                InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+        }
+    }
+}